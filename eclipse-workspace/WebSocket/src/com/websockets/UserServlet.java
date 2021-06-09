e52a36fa651e508bbd5a09de40f1fd34fcc11654
Shivani <shivanibhumagouni@gmail.com>
2021-06-09 19:58:53 +0530

    Project

---
+++ eclipse-workspace/WebSocket/src/com/websockets/UserServlet.java
@@ -0,0 +1,58 @@
+package com.websockets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/login")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    static RequestDispatcher rd;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter printWriter=response.getWriter();
+		
+		HttpSession session=request.getSession(true);
+		String username=request.getParameter("username");
+		session.setAttribute("username", username);
+		
+		if(username!=null && username.equalsIgnoreCase("doctor")) {
+			rd=request.getRequestDispatcher("/doctor.jsp");
+			rd.forward(request, response);
+		}
+		
+		else if(username!=null && username.equalsIgnoreCase("ambulance")) {
+			rd=request.getRequestDispatcher("/ambulance.jsp");
+			rd.forward(request, response);
+		}
+		
+		else if(username!=null) {
+			rd=request.getRequestDispatcher("/patient.jsp");
+			rd.forward(request, response);
+		}
+		
+	}
+
+}
